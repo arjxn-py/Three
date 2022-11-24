34271cbe054acb5b16448b50bb695eb3d5853eea
arjxn-py <arjxn.py@gmail.com>
2022-11-24 13:47:20 +0530

    Basic Box Added

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,26 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
+
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth/window.innerHeight,
+    0.1,
+    1000
+);
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+camera.position.set(1,2,5);
+
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const box = new THREE.Mesh(boxGeometry,boxMaterial);
+
+scene.add(box);
+
+renderer.render(scene , camera);
