a100777cf7467614449a3489e874cd241184155f
itAmircic <arjxn.py@gmail.com>
2022-12-01 20:05:48 +0530

    Added Random Plane Geometry

--- dist/index.64a4978e.js
+++ dist/index.64a4978e.js
@@ -538,16 +538,89 @@ const renderer = new _three.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 const scene = new _three.Scene();
-scene.background = new _three.Color(0xff0000);
+scene.background = new _three.Color(0xbbbfff);
 const camera = new _three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const geometry = new _three.PlaneGeometry(1, 1);
+const geometry = new _three.BufferGeometry();
+const vertices = new Float32Array([
+    // (North Face)
+    .5,
+    -0.5,
+    0,
+    1,
+    -1,
+    0,
+    1,
+    0,
+    0,
+    // (East Face)
+    .5,
+    -0.5,
+    0,
+    0,
+    -1,
+    0,
+    1,
+    -1,
+    0,
+    // (South Face)
+    .5,
+    -0.5,
+    0,
+    0,
+    0,
+    0,
+    0,
+    -1,
+    0,
+    // (West Face)
+    .5,
+    -0.5,
+    0,
+    1,
+    0,
+    0,
+    0,
+    0,
+    0,
+    // (North Face)
+    2,
+    -0.5,
+    0,
+    1,
+    -1,
+    0,
+    1,
+    0,
+    0,
+    // (North Face)
+    0,
+    -0.5,
+    0,
+    1,
+    -1,
+    0,
+    1,
+    0,
+    0,
+    // (North Face)
+    0,
+    -0.5,
+    1,
+    1,
+    -1,
+    1,
+    1,
+    0,
+    1
+]);
+geometry.setAttribute("position", new _three.BufferAttribute(vertices, 3));
 const material = new _three.MeshBasicMaterial({
-    color: 0xffff00,
-    side: _three.DoubleSide
+    color: 0xffffff,
+    wireframe: true
 });
-const dplane = new _three.Mesh(geometry, material);
-scene.add(dplane);
-dplane.position.set(3, 3, 3);
+const mesh = new _three.Mesh(geometry, material);
+mesh.rotation.x = -Math.PI / 2;
+scene.add(mesh);
 const orbit = new (0, _orbitControls.OrbitControls)(camera, renderer.domElement);
 const axesHelper = new _three.AxesHelper(5);
 scene.add(axesHelper);
@@ -558,7 +631,7 @@ const boxMaterial = new _three.MeshBasicMaterial({
     color: 0x00ff00
 });
 const box = new _three.Mesh(boxGeometry, boxMaterial);
-scene.add(box);
+// scene.add(box);
 const planeGeometry = new _three.PlaneGeometry(10, 10);
 const planeMaterial = new _three.MeshBasicMaterial({
     color: 0xbbaa66,
@@ -566,16 +639,16 @@ const planeMaterial = new _three.MeshBasicMaterial({
 });
 const plane = new _three.Mesh(planeGeometry, planeMaterial);
 plane.rotation.x = -0.5 * Math.PI;
-scene.add(plane);
+// scene.add(plane);
 const gridHelper = new _three.GridHelper(10);
 scene.add(gridHelper);
 const animate = (time)=>{
     box.rotation.x = time / 1000;
     box.rotation.y = time / 1000;
     box.rotation.z = time / 1000;
-    dplane.rotation.x = time / 1000;
-    dplane.rotation.y = time / 1000;
-    dplane.rotation.z = time / 1000;
+    // mesh.rotation.x = time/1000;
+    // mesh.rotation.y = time/1000; 
+    // mesh.rotation.z = time/1000;
     renderer.render(scene, camera);
 };
 renderer.setAnimationLoop(animate);
